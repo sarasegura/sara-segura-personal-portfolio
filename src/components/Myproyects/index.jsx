c2b2f5f2ba036d68a3157dd54950bb1803297cd5
sara segura <sarasegura224@gmail.com>
2023-11-18 14:35:10 -0500

    se agrego estilos al conponente Myproyects

--- src/components/Myproyects/index.jsx
+++ src/components/Myproyects/index.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./Myproyects.css";
 
-function Myproyects() {
-  return <p>soy Myproyects</p>;
-}
+const Myproyects = () => {
+  return (
+    <div className="container-myproyect">
+      <h3 className="title-myproyects">Mis proyectos</h3>
+      <div className="container-cart-proyects">
+        <span className="container-cart-proyect">
+          <img
+            className="img-proyects"
+            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
+            alt="Mi Imagen"
+          />
+          <p className="name-proyects">Video juego encuentra la miel</p>
+          <a className="enlace-proyect-p" href="">
+            Ver proyecto
+          </a>
+        </span>
+        <span className="container-cart-proyect">
+          <img
+            className="img-proyects"
+            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
+            alt="Mi Imagen"
+          />
+          <p className="name-proyects">Pagina de peluculas</p>
+          <a className="enlace-proyect-p" href="">
+            Ver proyecto
+          </a>
+        </span>
+        <span className="container-cart-proyect">
+          <img
+            className="img-proyects"
+            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
+            alt="Mi Imagen"
+          />
+          <p className="name-proyects">App page de notas</p>
+          <a className="enlace-proyect-p" href="">
+            Ver proyecto
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Myproyects;
