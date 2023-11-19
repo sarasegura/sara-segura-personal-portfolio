4861aecb26a78501add4563368018e849e84e6b2
sara segura <sarasegura224@gmail.com>
2023-11-18 20:05:24 -0500

    se agregaron estilos a el componente may proyect.

--- src/components/Myproyects/index.jsx
+++ src/components/Myproyects/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Myproyects.css";
 
+import paginadepeliculas from "../../assets/paginademovies.png";
+import juego from "../../assets/juegodeencontrarlamiel.png";
+import paginanotes from "../../assets/apppagenotes.png";
+
 const Myproyects = () => {
   return (
     <div className="container-myproyect">
       <h3 className="title-myproyects">Mis proyectos</h3>
       <div className="container-cart-proyects">
         <span className="container-cart-proyect">
-          <img
-            className="img-proyects"
-            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
-            alt="Mi Imagen"
-          />
+          <img className="img-proyects" src={juego} alt="Mi Imagen" />
           <p className="name-proyects">Video juego encuentra la miel</p>
           <a className="enlace-proyect-p" href="">
             Ver proyecto
@@ -20,20 +20,16 @@ const Myproyects = () => {
         <span className="container-cart-proyect">
           <img
             className="img-proyects"
-            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
+            src={paginadepeliculas}
             alt="Mi Imagen"
           />
-          <p className="name-proyects">Pagina de peluculas</p>
+          <p className="name-proyects">Pagina de peliculas</p>
           <a className="enlace-proyect-p" href="">
             Ver proyecto
           </a>
         </span>
         <span className="container-cart-proyect">
-          <img
-            className="img-proyects"
-            src="https://www.shutterstock.com/image-photo/beauty-glamour-girl-portrait-perfect-260nw-1906240834.jpg"
-            alt="Mi Imagen"
-          />
+          <img className="img-proyects" src={paginanotes} alt="Mi Imagen" />
           <p className="name-proyects">App page de notas</p>
           <a className="enlace-proyect-p" href="">
             Ver proyecto
