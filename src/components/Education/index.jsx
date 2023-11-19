b0a9104a7ddcd8b6ba09a3ca4a8785fa5292c8a9
sara segura <sarasegura224@gmail.com>
2023-11-19 11:45:25 -0500

    se agregaron estilos al componente education

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 import "./Education.css";
 
+import gitygithub from "../../assets/certificadogitygithub.png";
+import Javascript from "../../assets/certificadojavascript.png";
+
 function Education({ id }) {
   return (
     <div className="container-education" id={id}>
-      <h2 className="education-title">Educación</h2>
-      <span className="wrapper-card-education">
-        <div className="profession-name">
-          <p className="number-education">1</p>
-          <p className="p-education">
-            Tecnología en gestión de recursos naturales
-          </p>
-        </div>
-        <p className="p-education">SENA</p>
-        <p className="year-education">2018-2020</p>
-      </span>
-      <span className="wrapper-card-education">
-        <div className="profession-name">
-          <p className="number-education">2</p>
-          <p className="p-education">Front-end developer</p>
-        </div>
-        <p className="p-education">PLatzi</p>
-        <p className="year-education">2023</p>
-      </span>
+      <div className="wrapper-education">
+        <h2 className="education-title">Educación</h2>
+        <span className="wrapper-card-education">
+          <div className="profession-name">
+            <p className="number-education">1</p>
+            <p className="p-education">
+              Tecnología en gestión de recursos naturales
+            </p>
+          </div>
+          <p className="p-education">SENA</p>
+          <p className="year-education">2018-2020</p>
+        </span>
+        <span className="wrapper-card-education">
+          <div className="profession-name">
+            <p className="number-education">2</p>
+            <p className="p-education">Front-end developer</p>
+          </div>
+          <p className="p-education">PLatzi</p>
+          <p className="year-education">2023</p>
+        </span>
+      </div>
+      <div className="certificate-container">
+        <h2 className="certificate-title">Certificados</h2>
+        <span className="wrapper-certificate">
+          <span>
+            <img className="certificate-image" src={gitygithub} alt="" />
+            <p className="certificate-p">Curso profesional de GIT y GITHUB</p>
+          </span>
+          <span>
+            <img className="certificate-image" src={Javascript} alt="" />
+            <p className="certificate-p">Javascript a profundidad</p>
+          </span>
+          <span>
+            <img className="certificate-image" src={gitygithub} alt="" />
+            <p className="certificate-p">React a profundidad</p>
+          </span>
+        </span>
+      </div>
     </div>
   );
 }
